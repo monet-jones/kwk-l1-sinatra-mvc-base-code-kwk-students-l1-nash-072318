6281742313d3e2b35375e169b15b8b100c7a77ca
monet-jones <monet.s.jones@gmail.com>
2018-08-01 16:43:42 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,21 @@
+
 require 'bundler'
 Bundler.require
 
 class MyApp < Sinatra::Base
 
   get '/' do
+    @name="Monet loves Nashville KWK <3 "
     erb :index
   end
+  
+  
+    get'/chocolate' do 
+    erb :chocolate
+  end 
+  
+  get '/surfing' do
+    erb :surfing 
+  end 
 
 end
